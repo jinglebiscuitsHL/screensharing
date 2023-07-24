68fe603d5220e7061361e1cac279f3540e7c1053
Scott Wehby <scott.wehby@helplightning.com>
2023-07-24 15:24:15 -0500

    it works

--- app/src/main/java/com/scottwehby/screensharing/MainActivity.kt
+++ app/src/main/java/com/scottwehby/screensharing/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.scottwehby.screensharing
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.hardware.display.DisplayManager
 import android.hardware.display.VirtualDisplay
 import android.media.MediaRecorder
@@ -9,6 +11,8 @@ import android.media.projection.MediaProjectionManager
 import android.os.Build
 import android.os.Bundle
 import android.util.DisplayMetrics
+import android.util.Log
+import android.view.Surface
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
@@ -19,10 +23,12 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import java.io.File
+import java.io.IOException
+
 
 class MainActivity : ComponentActivity() {
 
@@ -51,6 +57,10 @@ class MainActivity : ComponentActivity() {
         } else {
             startService(Intent(this, ScreenCaptureService::class.java))
         }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO), 7)
+        }
+
         setContent {
             ScreenShareApp(
                 onStartCapture = {
@@ -68,29 +78,64 @@ class MainActivity : ComponentActivity() {
     }
 
     private fun startRecording() {
+        mediaRecorder = MediaRecorder()
+        mediaRecorder?.let {
+            prepareMediaRecorder(it)
+        }
         val metrics = DisplayMetrics()
         windowManager.defaultDisplay.getMetrics(metrics)
 
-        mediaRecorder = MediaRecorder().apply {
-            setVideoSource(MediaRecorder.VideoSource.SURFACE)
-            setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
-            val filePath = File(filesDir, "screen-capture.mp4").absolutePath
-            setOutputFile(filePath)
-            setVideoEncoder(MediaRecorder.VideoEncoder.H264)
-            setVideoSize(metrics.widthPixels, metrics.heightPixels)
-            setVideoFrameRate(30)
-            setVideoEncodingBitRate(3 * 1024 * 1024)
-            prepare()
-        }
-
+//        mediaRecorder?.apply {
+//            try {
+//                prepare()
+//                start()
+//                Thread.sleep(500)
+//                stop()
+//                reset()
+//                release()
+//                Log.d(TAG, "Microphone is available")
+//            } catch (e: Exception) {
+//                Log.e(TAG, "Microphone is not available", e)
+//            }
+//        }
         mediaProjection?.let {
             virtualDisplay = it.createVirtualDisplay(
                 "ScreenCapture",
-                metrics.widthPixels, metrics.heightPixels, metrics.densityDpi,
+                1080, 2200, metrics.densityDpi,
                 DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
                 mediaRecorder?.surface, null, null
             )
             mediaRecorder?.start()
+        }
+    }
+
+    private fun prepareMediaRecorder(mR: MediaRecorder) {
+        val metrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(metrics)
+        mR.apply {
+            val file = File(cacheDir, "screen-capture.mp4")
+            if (!file.exists()) {
+                Log.e(TAG, "Output file does not exist")
+            } else {
+                Log.d(TAG, "file exists: ${file.absolutePath}")
+            }
+            if (!file.canWrite()) {
+                Log.e(TAG, "Cannot write to output file")
+            }
+            Log.i("CAMERA", "Recording")
+            // Creating MediaRecorder and specifying video source, output format, encoder, and output file
+
+            setVideoSource(MediaRecorder.VideoSource.SURFACE)
+            setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+            Log.d(TAG, "setVideoSize: ${metrics.widthPixels} x ${metrics.heightPixels}")
+            setVideoSize(1080, 2200)
+            setVideoEncoder(MediaRecorder.VideoEncoder.DEFAULT)
+            setOutputFile(file.absolutePath)
+
+
+            prepare()
+            val surface: Surface = surface
+//            start()
         }
     }
 
@@ -100,6 +145,10 @@ class MainActivity : ComponentActivity() {
         mediaRecorder?.release()
         virtualDisplay?.release()
         mediaProjection?.stop()
+    }
+
+    companion object {
+        const val TAG = "MainActivity"
     }
 }
 
